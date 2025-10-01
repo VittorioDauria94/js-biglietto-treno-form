3f898a7ec3997e408d8c987a834a1ecc9e92caa0
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-10-01 17:37:53 +0200

    feat: completed HTML structure for Boolwarts Express page

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@
 const formElem = document.getElementById("ticket-form");
 
 //Input
-const destinationInput = document.getElementById("destination");
+const distanceInput = document.getElementById("distance");
 const selectAgeInput = document.getElementById("age");
+const destinationInput = document.getElementById("destination");
+
+//Elementi della card
 
 // Variabili globali
 const priceKm = 0.21;
@@ -17,17 +20,17 @@ let message = "";
 formElem.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const destinationElem = parseInt(destinationInput.value);
+  const distanceElem = parseInt(distanceInput.value);
   const ageElem = selectAgeInput.value;
   
   if (ageElem === "over-65") {
-    ticketPrice = (destinationElem * priceKm) * (1 - discountOver65);
+    ticketPrice = (distanceElem * priceKm) * (1 - discountOver65);
     message = `Il costo finale del biglietto sarà € ${ticketPrice.toFixed(2)}`;
     } else if (ageElem === "adult") {
-    ticketPrice = destinationElem * priceKm;
+    ticketPrice = distanceElem * priceKm;
     message = `Il costo finale del biglietto sarà € ${ticketPrice.toFixed(2)}`;
     } else {
-    ticketPrice = (destinationElem * priceKm) * (1 - discountUnderage);
+    ticketPrice = (distanceElem * priceKm) * (1 - discountUnderage);
     message = `Il costo finale del biglietto sarà € ${ticketPrice.toFixed(2)}`;
 }
     console.log(message);
